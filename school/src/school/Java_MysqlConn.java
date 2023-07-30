a21f95250bd356ef557e9f92217b37983a573d29
ABDULMAJEED IBRAHIM ABDULLAH ABUABADAH <129085964+abdulmajeedib@users.noreply.github.com>
2023-07-30 05:00:17 -0700

    Add files via upload

---
+++ school/src/school/Java_MysqlConn.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package school;
+
+/**
+ *
+ * @author 96655
+ */
+class Java_MysqlConn {
+    
+}
